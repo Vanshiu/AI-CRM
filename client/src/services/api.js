8f7e2329054bc416ad61b9588a61d6c5dc6f7b13
Vansh Bhardwaj <vanshbhardwaj1969@gmail.com>
2026-05-21 21:57:56 +0530

    Configure production API endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a centralized Axios instance
 const api = axios.create({
-  baseURL: '/api', // Vite dev proxy redirects /api → http://localhost:5000/api
+  baseURL: import.meta.env.VITE_API_URL + '/api', // Vite dev proxy redirects /api → http://localhost:5000/api
   headers: {
     'Content-Type': 'application/json'
   }
